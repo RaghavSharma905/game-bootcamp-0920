fde6dc420760ce7bc910aa785615c405b88bf380
RaghavSharma905 <72238870+RaghavSharma905@users.noreply.github.com>
2020-10-02 09:16:24 +0530

    Update basic.js

--- lecture-1/basics/basic.js
+++ lecture-1/basics/basic.js
@@ -1,20 +1,25 @@
 demo()
 
-
-const x = 2;
+ 
+const x = 2; //here const is used in  varible declaration that can  not be changed again.
 console.log(x);
 // x = 5;
 
-function demo() {
+function demo() { 
     console.log("In function demo");
 }
 
-demo();
+demo();// function is called
 
-if (1 == 2) {
+if (1 == 2)
+{
     console.log("In if block")
-} else if (1 < 5) {
+} 
+else if (1 < 5)
+{
     console.log("In else-if block")
-} else {
+}
+else
+{
     console.log("In else block")
 }
